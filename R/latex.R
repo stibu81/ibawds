4de8f89e1efd118f0dbd7ff04f138f6245dbbb29
stibu81 <slanz1137@gmail.com>
2021-01-10 17:53:57 +0100

    add define_latex_stats()

---
+++ R/latex.R
@@ -0,0 +1,28 @@
+#' Define LaTeX commands for statistical symbols
+#'
+#' Use this function to add the definitions for various useful
+#' equation symbols for statistics to a RMarkdown document.
+#'
+#' @details
+#' Run this function from within a code chunk with options
+#' `results = "asis"` and `echo = FALSE`.
+#'
+#' It adds the following commands: `\E`, `\P`, `\Var`,
+#' `\SD`, `\SE`, `\Xb`, `\Yb`
+#'
+#' @export
+
+define_latex_stats <- function() {
+
+  cat("\\newcommand{\\E}{\\mbox{E}}",
+      # original \P stands for pilcrow
+      "\\renewcommand{\\P}{\\mbox{P}}",
+      "\\newcommand{\\Var}{\\mbox{Var}}",
+      "\\newcommand{\\SD}{\\mbox{SD}}",
+      "\\newcommand{\\SE}{\\mbox{SE}}",
+      "\\newcommand{\\Xb}{\\bar{X}}",
+      "\\newcommand{\\Yb}{\\bar{Y}}",
+      sep = "\n")
+
+  invisible(NULL)
+}
