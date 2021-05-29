963264b16712d3b8e158a6dbd58d33bdbce537c8
stibu81 <slanz1137@gmail.com>
2021-05-29 17:48:28 +0200

    improve script that generates cran_history

--- data-raw/cran_history.R
+++ data-raw/cran_history.R
@@ -3,9 +3,12 @@ library(dplyr)
 # get number of available CRAN packages from MRAN for every quarter
 # since 2014-10-01. Do not redownload the quarters that are already
 # available in the package
-dates <- seq(as.Date("2014-10-01"), lubridate::today(), by = "3 months") %>%
+start_date <- as.Date("2014-10-01")
+cran_history <- get_cran_history() %>%
+  filter(date >= start_date)
+dates <- seq(start_date, lubridate::today(), by = "3 months") %>%
   as.character %>%
-  setdiff(as.character(ibawds:::cran_history$date)) %>%
+  setdiff(as.character(cran_history$date)) %>%
   as.Date()
 
 if (length(dates) == 0) {
@@ -19,4 +22,4 @@ cran_history_new <- tibble(
 
 cran_history <- bind_rows(cran_history, cran_history_new)
 
-usethis::use_data(cran_history, internal = TRUE)
+usethis::use_data(cran_history, internal = TRUE, overwrite = TRUE)
