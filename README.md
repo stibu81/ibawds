d34f0388eba29fd18cd7b2435db5d0a7cee84cd4
stibu81 <slanz1137@gmail.com>
2021-10-10 19:42:40 +0200

    add hex sticker

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
-# ibawds
+# ibawds <img src="man/figures/ibawds_logo.png" align="right" height="175" />
 
 <!-- badges: start -->
 
---
+++ man/figures/create_sticker.R
@@ -0,0 +1,13 @@
+
+hexSticker::sticker(
+  devtools::package_file("man", "figures", "ibaw.png"),
+  package = "ibawds",
+  p_size = 22, p_color = "black",
+  p_family = "",
+  p_x = 1.3, p_y = 1.35,
+  s_x = 1, s_y = 0.8, s_width = .8,
+  h_fill = "white",
+  h_color = rgb(0, 0.69, 0.77),
+  h_size = 1.5,
+  filename = devtools::package_file("man", "figures", "ibawds_logo.png")
+)
