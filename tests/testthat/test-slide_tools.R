1fcdedaa2f4e15aa856a3bb9ab518ed28f32d3bd
stibu81 <slanz1137@gmail.com>
2021-12-12 17:18:14 +0100

    add better tests for define_latex_stats()

--- tests/testthat/test-slide_tools.R
+++ tests/testthat/test-slide_tools.R
@@ -1,11 +1,46 @@
 library(ggplot2)
+library(knitr)
+library(stringr)
+library(rmarkdown)
 
-test_that("test define_latex_stats()", {
-  expect_output(
-    expect_invisible(out <-define_latex_stats(), "\\\\newcommand\\{")
-  )
+
+test_that("check that define_latex_stats() creates output", {
+  expect_output(expect_invisible(out <-define_latex_stats()))
   expect_null(out)
 })
+
+# test knitting with define_latex_stats()
+commands <- paste0("\\",
+                   c("E", "P", "Var", "Cov", "Cor", "SD", "SE", "Xb", "Yb"))
+txt <- paste("---
+output:
+  pdf_document: default
+---
+
+```{r results = \"asis\", echo = FALSE}
+  ibawds::define_latex_stats()
+```
+$$", paste(commands, collapse = " "), "$$")
+res <- knitr::knit(text = txt, quiet = TRUE)
+
+
+test_that("check that define_latex_stats() defines all the expected commands", {
+  # check output when knitting
+  expect_type(res, "character")
+  expect_equal(
+    str_detect(res, paste0("\\\\(re)?newcommand\\{\\", commands, "\\}\\{.*\\}")),
+    rep(TRUE, length(commands))
+  )
+
+  # check rendering  a document
+  rmd_file <- tempfile(fileext = ".Rmd")
+  pdf_file <- str_replace(rmd_file, "Rmd", "pdf")
+  on.exit(unlink(c(rmd_file, pdf_file)))
+  writeLines(txt, rmd_file)
+  expect_silent(render(rmd_file, quiet = TRUE))
+  expect_true(file.exists(pdf_file))
+})
+
 
 # store current options as reference
 ref_opts <- list(ggplot = theme_get(),
