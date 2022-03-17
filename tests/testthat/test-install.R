a303903078133296ea8b8e801e8fc284977f36e4
stibu81 <slanz1137@gmail.com>
2022-03-17 18:48:37 +0100

    fix warning in test

--- tests/testthat/test-install.R
+++ tests/testthat/test-install.R
@@ -6,7 +6,7 @@ test_that("check get_required_packages()", {
             "kableExtra", "tidyverse", "rmarkdown", "caret", "reshape2",
             "lubridate", "ggrepel", "writexl", "cowplot", "DT", "gutenbergr",
             "tidytext", "rvest", "Lahman", "pdftools", "HistData", "titanic",
-            "BiocManager", "waldo")
+            "BiocManager", "waldo", "knitr", "hexbin", "patchwork", "GGally")
   expect_true(all(pkgs %in% req_pkgs))
   expect_false(any(c("R", "testthat", "usethis", "vdiffr", "covr") %in% req_pkgs))
 })
@@ -14,7 +14,8 @@ test_that("check get_required_packages()", {
 # this test only succeeds on systems, where all the required
 # packages are installed.
 test_that("test install_ibawds()", {
-  skip_if_not(rlang::is_installed(ibawds:::get_required_packages()),
+  req_pkgs <- get_required_packages()
+  skip_if_not(suppressMessages(rlang::is_installed(req_pkgs)),
               "not all the required packages are installed.")
   expect_message(install_ibawds(), "All the required packages are installed.")
 })
