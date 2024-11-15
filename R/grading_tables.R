05bd346ebb43fdcb2aa7492172f86f720cb12f5a
stibu81 <slanz1137@gmail.com>
2024-11-15 21:34:56 +0100

    add wordlist for spell_check_evaluation()

--- R/grading_tables.R
+++ R/grading_tables.R
@@ -124,11 +124,16 @@ create_grading_table <- function(p_text, p_tab, p_plot, p_code, p_stat) {
 #' @param path path to the top level directory of the evaluations.
 #' @param students an optional character vector with student names. If given,
 #'  only the evaluation of these students will be checked.
+#' @param use_wordlist should a list of words be excluded from the spell
+#'  check? The package contains a list of words that have typically appeared
+#'  in the evaluations of the past. The names of the students will always be
+#'  excluded from spell check, even if `use_wordlist` is `FALSE`.
 #'
 #' @export
 
 spell_check_evaluation <- function(path = ".",
-                                   students = NULL) {
+                                   students = NULL,
+                                   use_wordlist = TRUE) {
 
   # find the evaluation files recursively, ignore the template
   eval_files <- list.files(path, "Beurteilung_.*\\.Rmd",
@@ -149,6 +154,14 @@ spell_check_evaluation <- function(path = ".",
   # extract the names of the students and use them as the ignore list.
   ignore_list <- eval_files %>%
     stringr::str_extract("Beurteilung_(.*)\\.Rmd", group = 1)
+
+  # add words from the wordlist to the ignore list, if requested
+  if (use_wordlist) {
+    ignore_list <- system.file("extdata", "evaluation_wordlist",
+                               package = "ibawds") %>%
+      readLines() %>%
+      c(ignore_list)
+  }
 
   # run the spell check
   spelling::spell_check_files(eval_files, lang = "de_CH",
