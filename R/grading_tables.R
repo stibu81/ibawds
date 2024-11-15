fbe73eda815a13d6ea38993bc1f9ac427ea54751
stibu81 <slanz1137@gmail.com>
2024-11-15 21:01:47 +0100

    add spell_check_evaluation()

--- R/grading_tables.R
+++ R/grading_tables.R
@@ -112,3 +112,48 @@ create_grading_table <- function(p_text, p_tab, p_plot, p_code, p_stat) {
     kableExtra::row_spec(6, bold = TRUE, background = grDevices::gray(0.8))
 
 }
+
+
+# nocov start
+
+#' Check Spelling in the Evaluation of the Papers
+#'
+#' Evaluation of the student papers is done in the form of an Rmd file. This
+#' function finds all the evaluations in a directory and checks the spelling.
+#'
+#' @param path path to the top level directory of the evaluations.
+#' @param students an optional character vector with student names. If given,
+#'  only the evaluation of these students will be checked.
+#'
+#' @export
+
+spell_check_evaluation <- function(path = ".",
+                                   students = NULL) {
+
+  # find the evaluation files recursively, ignore the template
+  eval_files <- list.files(path, "Beurteilung_.*\\.Rmd",
+                           recursive = TRUE,
+                           full.names = TRUE) %>%
+    stringr::str_subset("Beurteilung_Template\\.Rmd", negate = TRUE)
+
+  if (!is.null(students)) {
+    eval_files <- eval_files %>%
+      stringr::str_subset(paste0("Beurteilung_", students, ".Rmd", collapse = "|"))
+  }
+
+  # abort, if there are not files to check
+  if (length(eval_files) == 0) {
+    cli::cli_abort("No evaluation files found.")
+  }
+
+  # extract the names of the students and use them as the ignore list.
+  ignore_list <- eval_files %>%
+    stringr::str_extract("Beurteilung_(.*)\\.Rmd", group = 1)
+
+  # run the spell check
+  spelling::spell_check_files(eval_files, lang = "de_CH",
+                              ignore = ignore_list)
+
+}
+
+# nocov end
