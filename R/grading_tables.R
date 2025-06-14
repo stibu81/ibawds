bc3a93a73bd374d3c3c1de8586103e3f93318e01
stibu81 <slanz1137@gmail.com>
2025-06-14 18:07:20 +0200

    add multi-language support for grading tables

--- R/grading_tables.R
+++ R/grading_tables.R
@@ -14,6 +14,8 @@
 #' @param p_code numeric between 0 and 5, points given for the code
 #' @param p_stat numeric between 0 and 5, points given for the
 #'  statistic computations
+#' @param lang language to use in the tables. Supported languages are
+#'  German (`"de"`, the default) and English ("`en"`).
 #'
 #' @details
 #' The tables are created using [`knitr::kable()`] and [`kableExtra::kableExtra`] is
@@ -45,11 +47,14 @@
 #' @name grading_tables
 #' @export
 
-create_minreq_table <- function(repro, n_tab, n_plot_kinds, n_plots, n_stat) {
+create_minreq_table <- function(repro, n_tab, n_plot_kinds, n_plots, n_stat,
+                                lang = c("de", "en")) {
 
   rlang::check_installed("kableExtra")
 
-  yes_no <- function(l) c("Nein", "Ja")[l + 1]
+  lang <- rlang::arg_match(lang)
+
+  yes_no <- function(l) tab_labs(c("Nein", "Ja"), lang, TRUE)[l + 1]
   nok_ok <- function(l) c("NOK", "OK")[l + 1]
 
   # don't accept negative points
@@ -60,23 +65,26 @@ create_minreq_table <- function(repro, n_tab, n_plot_kinds, n_plots, n_stat) {
   # vector of minimal requirements
   min_req <- c(1, 1, 3, 5, 2)
   # titles of the requirements
-  req_titles <- c("Reproduzierbarkeit", "Tabellen",
-                  "Arten Plots", "Plots",
-                  "stat. Auswertungen")
+  req_titles <- c(
+    tab_labs("Reproduzierbarkeit", lang, TRUE),
+    tab_labs(c("Tabellen", "Arten Plots", "Plots", "stat. Auswertungen"), lang)
+  )
 
   dplyr::tibble(
-    Anforderung = paste(c("", "\u2265", "\u2265", "\u2265", "\u2265"),
+    requirement = paste(c("", "\u2265", "\u2265", "\u2265", "\u2265"),
                         c("", min_req[-1]),
                         req_titles),
     res = c(repro, n_tab, n_plot_kinds, n_plots, n_stat),
-    Beurteilung = c(nok_ok(.data$res[1]),
-                    paste(.data$res[-1], req_titles[-1])),
+    assessment = c(nok_ok(.data$res[1]),
+                   paste(.data$res[-1], req_titles[-1])),
     # setting the name with umlaut here leadds to a CRAN warning on Windows
     # => use no umlaut here, rename below
-    Erfullt = yes_no(.data$res >= min_req)
+    satisfied = yes_no(.data$res >= min_req)
   ) %>%
   dplyr::select(-"res") %>%
-  magrittr::set_names(c("Anforderung", "Beurteilung", "Erf\u00fcllt")) %>%
+  magrittr::set_names(
+    tab_labs(c("Anforderung", "Beurteilung", "Erf\u00fcllt"), lang, TRUE)
+  ) %>%
   kableExtra::kable(format = "html") %>%
   kableExtra::kable_styling()
 }
@@ -85,9 +93,12 @@ create_minreq_table <- function(repro, n_tab, n_plot_kinds, n_plots, n_stat) {
 #' @name grading_tables
 #' @export
 
-create_grading_table <- function(p_text, p_tab, p_plot, p_code, p_stat) {
+create_grading_table <- function(p_text, p_tab, p_plot, p_code, p_stat,
+                                 lang = c("de", "en")) {
 
   rlang::check_installed("kableExtra")
+
+  lang <- rlang::arg_match(lang)
 
   points <- c(p_text, p_tab, p_plot, p_code, p_stat)
   max <- c(3, 3, 5, 5, 5)
@@ -97,19 +108,71 @@ create_grading_table <- function(p_text, p_tab, p_plot, p_code, p_stat) {
   }
 
   dplyr::tibble(
-    Titel = c("Text", "Tabellen", "Plots", "Code", "Stat. Auswertungen"),
-    Punkte = points,
-    Von = max,
-    Prozent = scales::label_percent(accuracy = 1)(.data$Punkte / .data$Von)
-  ) %>%
+      title = tab_labs(
+        c("Text", "Tabellen", "Plots", "Code", "stat. Auswertungen"),
+        lang = lang,
+        cap = TRUE
+      ),
+      points = points,
+      max = max
+    ) %>%
     dplyr::bind_rows(
       dplyr::tibble(
-        Titel = "Total", Punkte = sum(.$Punkte),
-        Von = sum(.$Von),
-        Prozent = scales::label_percent(accuracy = 1)(.data$Punkte / .data$Von))
+        title = tab_labs("Total", lang, TRUE),
+        points = sum(.$points),
+        max = sum(.$max)
+      )
+    ) %>%
+    dplyr::mutate(
+      perc = scales::label_percent(accuracy = 1)(.data$points / .data$max)) %>%
+    purrr::set_names(
+      tab_labs(c("Titel", "Punkte", "Von", "Prozent"), lang, TRUE)
     ) %>%
     kableExtra::kable(format = "html") %>%
     kableExtra::kable_styling() %>%
     kableExtra::row_spec(6, bold = TRUE, background = grDevices::gray(0.8))
 
+}
+
+
+# define the text labels in German and English
+table_texts <- tibble::tribble(
+                   ~de,                  ~en,
+                "Text",               "text",
+            "Tabellen",             "tables",
+               "Plots",              "plots",
+                "Code",               "code",
+  "stat. Auswertungen", "stat. computations",
+               "Total",              "total",
+  "Reproduzierbarkeit",    "reproducibility",
+         "Arten Plots",     "kinds of plots",
+         "Anforderung",        "requirement",
+         "Beurteilung",         "assessment",
+        "Erf\u00fcllt",          "satisfied",
+               "Titel",              "title",
+              "Punkte",             "points",
+                 "Von",                "max",
+             "Prozent",         "percentage",
+                  "Ja",                "yes",
+                "Nein",                 "no"
+  )
+
+
+tab_labs <- function(texts, lang, cap = FALSE,
+                     error_call = rlang::caller_env()) {
+
+  # find the values in the table
+  it <- match(texts, table_texts$de)
+
+  # abort, if one of the texts is not found
+  if (any(is.na(it))) {
+    cli::cli_abort("invalid text(s) requested: {texts[is.na(it)]}",
+                   call = error_call)
+  }
+
+  labs <- table_texts[[lang]][it]
+
+  if (cap) labs <- stringr::str_to_sentence(labs, locale = lang)
+
+  labs
 }
--- tests/testthat/test-grading_tables.R
+++ tests/testthat/test-grading_tables.R
@@ -2,8 +2,8 @@ library(rvest)
 library(dplyr, warn.conflicts = FALSE)
 library(magrittr, warn.conflicts = FALSE)
 
-# reference tables
-mrt1_ref <- tibble(
+# reference tables in German
+mrt1_ref_de <- tibble(
   Anforderung = c("Reproduzierbarkeit", "\u2265 1 Tabellen",
                   "\u2265 3 Arten Plots", "\u2265 5 Plots",
                   "\u2265 2 stat. Auswertungen"),
@@ -11,42 +11,70 @@ mrt1_ref <- tibble(
                   "2 stat. Auswertungen"),
   "Erf\u00fcllt" = "Ja"
 )
-mrt2_ref <- mrt1_ref %>%
+mrt2_ref_de <- mrt1_ref_de %>%
   mutate(
     Beurteilung = c("NOK", "2 Tabellen", "3 Arten Plots", "3 Plots",
                     "0 stat. Auswertungen"),
     "Erf\u00fcllt" = c("Nein", "Ja", "Ja", "Nein", "Nein")
 )
 
-grt1_ref <- tibble(
+grt1_ref_de <- tibble(
   Titel = c("Text", "Tabellen", "Plots", "Code", "Stat. Auswertungen", "Total"),
   Punkte = c(2.5, 2, 3, 1, 2, 10.5),
   Von = c(3L, 3L, 5L, 5L, 5L, 21L),
   Prozent = c("83%", "67%", "60%", "20%", "40%", "50%")
 )
 
+# reference tables in English
+
+mrt2_ref_en <- tibble(
+  Requirement = c("Reproducibility", "\u2265 1 tables",
+                  "\u2265 3 kinds of plots", "\u2265 5 plots",
+                  "\u2265 2 stat. computations"),
+  Assessment = c("NOK", "2 tables", "3 kinds of plots", "3 plots",
+                 "0 stat. computations"),
+  Satisfied = c("No", "Yes", "Yes", "No", "No")
+)
+
+grt1_ref_en <- grt1_ref_de %>%
+  mutate(
+    Titel = c("Text", "Tables", "Plots", "Code", "Stat. computations", "Total")
+  ) %>%
+  set_names(c("Title", "Points", "Max", "Percentage"))
+
 # helper function to parse the tables
-parse_tab <- . %>%
-    as.character() %>%
+parse_tab <- function(x) {
+    as.character(x) %>%
     read_html() %>%
     html_table() %>%
     extract2(1)
+}
 
 test_that("test create_minreq_table", {
   mrt1 <- create_minreq_table(TRUE, 3, 3, 5, 2)
   expect_s3_class(mrt1, "kableExtra")
-  expect_equal(parse_tab(mrt1), mrt1_ref)
+  expect_equal(parse_tab(mrt1), mrt1_ref_de)
 
   mrt2 <- create_minreq_table(FALSE, 2, 3, 3, 0)
   expect_s3_class(mrt2, "kableExtra")
-  expect_equal(parse_tab(mrt2), mrt2_ref)
+  expect_equal(parse_tab(mrt2), mrt2_ref_de)
 })
 
 
 test_that("test create_grading_table", {
   grt1 <- create_grading_table(2.5, 2, 3, 1, 2)
   expect_s3_class(grt1, "kableExtra")
-  expect_equal(parse_tab(grt1), grt1_ref)
+  expect_equal(parse_tab(grt1), grt1_ref_de)
+})
+
+test_that("test English tables", {
+  mrt2 <- create_minreq_table(FALSE, 2, 3, 3, 0, lang = "en")
+  expect_s3_class(mrt2, "kableExtra")
+  expect_equal(parse_tab(mrt2), mrt2_ref_en)
+
+  grt1 <- create_grading_table(2.5, 2, 3, 1, 2, lang = "en")
+  expect_s3_class(grt1, "kableExtra")
+  expect_equal(parse_tab(grt1), grt1_ref_en)
 })
 
 
@@ -59,4 +87,30 @@ test_that("test invalid inputs", {
                "invalid input")
   expect_error(create_minreq_table(TRUE, 1, -1, 1, 1),
                "invalid input")
+  expect_error(create_minreq_table(TRUE, 1, 1, 1, 1, lang = "xy"),
+               "`lang` must be one of")
+})
+
+
+test_that("tab_labs() works", {
+  expect_equal(tab_labs(c("Tabellen", "stat. Auswertungen"), lang = "de"),
+               c("Tabellen", "stat. Auswertungen"))
+  expect_equal(tab_labs(c("Tabellen", "stat. Auswertungen"), lang = "en"),
+               c("tables", "stat. computations"))
+
+  expect_equal(
+    tab_labs(c("Tabellen", "stat. Auswertungen"), lang = "de", cap = TRUE),
+    c("Tabellen", "Stat. Auswertungen")
+  )
+  expect_equal(
+    tab_labs(c("Tabellen", "stat. Auswertungen"), lang = "en", cap = TRUE),
+    c("Tables", "Stat. computations")
+  )
+})
+
+
+test_that("tab_labs() handles invalid input", {
+  expect_null(tab_labs(c("Tabellen", "stat. Auswertungen"), lang = "fr"))
+  expect_error(tab_labs(c("abc", "Tabellen", "def")),
+               "invalid text\\(s\\) requested: abc and def")
 })
